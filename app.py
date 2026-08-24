c784ec4ec8748be3fda483db01b6a014304206db
Maikon <maikon@guaralub.com>
2026-08-24 11:32:15 -0300

    atualiza link do relatorio loja

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ usuarios = {
     "ana": "Guara@vendas2026"
 }
 
-link_loja = "https://app.powerbi.com/links/cd07217e-dedd-4846-984b-9e561e0d826d?ctid=425c7c8c-859b-4c1b-9c2a-b609c6a8e14b"
+link_loja = "https://app.powerbi.com/groups/me/reports/f2ee6213-1533-410c-8ceb-f30bbafad857"
 link_vendedores = "https://app.powerbi.com/links/PsAlAMGCDf?ctid=425c7c8c-859b-4c1b-9c2a-b609c6a8e14b&pbi_source=linkShare"
 
 @app.route("/")
